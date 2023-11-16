d4bb4eb684e06a3b7e58b493189206b206de12dd
simminjeong <zngkgk97@gmail.com>
2023-11-16 17:18:50 +0900

    feat : user 프로필 조회 추가

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import Response
+from rest_framework.views import APIView
+from .models import Profile
+from .serializers import ProfileSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
-# Create your views here.
+
+class ProfileView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        profile = get_object_or_404(Profile, user=request.user)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+
+
