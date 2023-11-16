4d57be6f0ae826223ac9eb9cb82bab92e9a9ecfc
kimyeoju <yeojoo0031@gmail.com>
2023-11-16 16:28:03 +0900

    feat : User 시리얼라이즈 추가

---
+++ user/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email = validated_data['email'],
+            password = validated_data['password']
+        )
+        user.save()
+        return user
