80973a1f33797c40131a8e399af9566b9b20642a
simminjeong <zngkgk97@gmail.com>
2023-11-16 23:16:05 +0900

    fix : 오타 수정

--- user/serializers.py
+++ user/serializers.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['name', 'image', 'about', 'gnere']
+        fields = ['name', 'image', 'about', 'genre']
