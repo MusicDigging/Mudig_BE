827539de83f81f2f03861ad6689f1d84343a59b2
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-11-17 18:32:55 +0900

    feat : Deploy Setting

--- mudig/urls.py
+++ mudig/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('playlist/', include('playlist.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
