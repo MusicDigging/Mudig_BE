701fbfaf74a851d6f9f0c6ea76be2a02f3617c14
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-11-18 15:56:28 +0900

    feat : MusicSerializer 추가

--- playlist/serializers.py
+++ playlist/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Playlist
+from .models import Playlist, Music
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
+        fields = '__all__'
+
+
+class MusicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music
         fields = '__all__'
