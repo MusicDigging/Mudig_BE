ea2d8a83cf2113f7866aa335f0e20f1db2410d9a
kimyeoju <yeojoo0031@gmail.com>
2023-11-19 21:32:07 +0900

    chore : 회원가입 시 신규 유저와 기존 유저 구분 GenerateOtp 코드 수정

--- user/views.py
+++ user/views.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 from rest_framework.response import Response
+from .models import User
 from .utils import generate_otp, send_otp_via_email
 
 class Join(APIView):
@@ -24,16 +25,36 @@ class Join(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# class GenerateOtp(APIView):
+#     def post(self, request):
+#         email = request.data.get('email')
+#         if not email:
+#             return Response({'message': '이메일 주소를 입력하세요.'}, status=status.HTTP_400_BAD_REQUEST)
+#         try:
+#             user = User.objects.get(email=email)
+#         except:
+#             otp = generate_otp()
+#             send_otp_via_email(email, otp=otp)
+        
+#             response = {'message': '인증 번호 생성', 'otp': otp}
+#             return Response(data=response, status=status.HTTP_200_OK)
+#         else:
+#             return Response({'message': '이미 가입된 사용자입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        
 class GenerateOtp(APIView):
     def post(self, request):
         email = request.data.get('email')
-        if email:
+        if not email:
+            return Response({'message': '이메일 주소를 입력하세요'}, status=status.HTTP_400_BAD_REQUEST)
+        users = User.objects.filter(email=email)
+        if not users.exists():
             otp = generate_otp()
             send_otp_via_email(email, otp=otp)
             response = {'message': '인증 번호 생성', 'otp': otp}
             return Response(data=response, status=status.HTTP_200_OK)
         else:
-            return Response({'message': '이메일 주소를 입력하세요.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': '이미 가입된 사용자입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class Login(APIView):
