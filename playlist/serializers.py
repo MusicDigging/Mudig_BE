b0d89ac02b9ecf1f84819f204961b34fdddf1cb0
su12 <ts04109@naver.com>
2023-12-11 19:35:13 +0900

    fix : comment serializer 수정

--- playlist/serializers.py
+++ playlist/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Playlist, Music, Like, Comment
+from user.serializers import ProfileSerializer
 
 class PlaylistSerializer(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField()
     class Meta:
         model = Playlist
         fields = '__all__'
+        
+    def get_like_count(self, obj):
+        return obj.like_set.count()
     
     def get_playlist_info(self, obj):
         playlist_info = [{'id': playlist.id, 'thumbnail': playlist.thumbnail,'title':playlist.title} for playlist in obj]
@@ -22,9 +27,10 @@ class MusicSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    writer_profile = ProfileSerializer(source='writer.profile', read_only=True)
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'content', 'writer', 'writer_profile', 'created_at', 'updated_at']
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -32,4 +38,3 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = '__all__'
-
